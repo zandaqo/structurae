a597eb60ee31bc2170b917434546357832b16d14
Maga D. Zandaqo <denelxan@gmail.com>
2019-03-07 17:40:10 +0300

    chore: update dependencies

--- test/setup.js
+++
@@ -1,9 +0,0 @@
-const { TextEncoder, TextDecoder } = require('util');
-
-Object.defineProperty(global, 'TextEncoder', {
-  value: TextEncoder,
-});
-
-Object.defineProperty(global, 'TextDecoder', {
-  value: TextDecoder,
-});
