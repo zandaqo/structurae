bae7c579d6ae3848d06e10f7465f15cc96bf2bfc
Maga D. Zandaqo <denelxan@gmail.com>
2021-06-23 14:25:14 +0300

    fix: correctly link to sourcemaps

--- scripts/build.ts
+++ scripts/build.ts
@@ -12,14 +12,26 @@ try {
   for (const [fileName, text] of Object.entries(files)) {
     const isJS = fileName.endsWith(".js");
     const isDTS = !isJS && fileName.endsWith(".d.ts");
-    const newName = fileName.replace(/file\:\/\/\//g, "")
+    const name = fileName.substring(
+      fileName.lastIndexOf("/") + 1,
+      fileName.indexOf("."),
+    );
+    const path = fileName.replace(/file\:\/\/\//g, "")
       .replace(/\.ts\./g, "."); // remove .ts extenstion
     let content = text;
     if (isJS || isDTS) {
       content = text.replace(/\.ts";/g, isDTS ? '";' : '.js";') // remove .ts extension
         .replace(/^\/\/\/.*?\/>\s/g, ""); // remove triple slash comments
+      if (isJS) {
+        content += `\n//# sourceMappingURL=${name + ".js.map"}`; // add link to source file
+      }
+    } else if (path.endsWith(".js.map")) {
+      const source = JSON.parse(text);
+      source.file = name + ".js";
+      source.sources = [name + ".ts"];
+      content = JSON.stringify(source);
     }
-    Deno.writeTextFileSync(newName, content);
+    Deno.writeTextFileSync(path, content);
   }
   console.log("Done!");
 } catch (e) {
