58a37e6a7da0dc241ee36663cd8cf16ad11bfbbc
Maga D. Zandaqo <denelxan@gmail.com>
2022-07-11 19:59:54 +0400

    chore: update build script

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,7 +1,7 @@
 import {
   build,
   emptyDir,
-} from "https://raw.githubusercontent.com/denoland/dnt/0.23.0/mod.ts";
+} from "https://raw.githubusercontent.com/denoland/dnt/0.28.0/mod.ts";
 
 await emptyDir("./npm");
 
@@ -50,8 +50,15 @@ await build({
     bugs: {
       url: "https://github.com/zandaqo/structurae/issues",
     },
-    engines: {
-      node: ">=14.0.0",
+    exports: {
+      ".": {
+        types: "./types/index.d.ts",
+        import: "./esm/index.js",
+      },
+      "./*": {
+        types: "./types/*.d.ts",
+        import: "./esm/*.js",
+      },
     },
   },
 });
